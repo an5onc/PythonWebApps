628cdd1da6cd01fe0ffbe5a1b064b0c31da0248d
Anson Cordeiro <cord3576@bears.unco.edu>
2024-09-23 13:03:02 -0600

    Update views.py

--- student/Superhero/hero/views.py
+++ student/Superhero/hero/views.py
@@ -1,22 +1,68 @@
 from django.views.generic import TemplateView
 
-class HulkView(TemplateView):
+# Dictionary of heroes
+heroes = {
+    'hulk': {
+        'title': 'Hulk',
+        'id': 'Bruce Banner',
+        'strengths': 'Superhuman strength, invulnerability',
+        'weaknesses': 'Uncontrollable rage',
+        'image': '/static/hulk.jpeg'
+    },
+    'superman': {
+        'title': 'Superman',
+        'id': 'Clark Kent',
+        'strengths': 'Super strength, flight, invulnerability, laser vision',
+        'weaknesses': 'Kryptonite',
+        'image': '/static/superman.jpeg'
+    },
+    'batman': {
+        'title': 'Batman',
+        'id': 'Bruce Wayne',
+        'strengths': 'Martial arts, genius intellect, detective skills',
+        'weaknesses': 'Human limits',
+        'image': '/static/batman.jpeg'
+    },
+    'wonderwoman': {
+        'title': 'Wonder Woman',
+        'id': 'Diana Prince',
+        'strengths': 'Super strength, combat skills, Lasso of Truth',
+        'weaknesses': 'None known',
+        'image': '/static/wonderwoman.jpeg'
+    },
+    'store': {
+        'title': 'My Store',
+        'id': 'Interlock Go',
+        'strengths': 'Quick installation, reliability',
+        'weaknesses': 'Limited staff',
+        'image': '/static/interlockgo.jpeg'
+    }
+}
+
+# View to display the list of heroes
+class HeroListView(TemplateView):
     template_name = 'heroes.html'
 
     def get_context_data(self, **kwargs):
-        return {
-            'title': 'Business Card',
-            'id': 'Anson Cordeiro',
-            'image': '/static/businesscard.jpeg'
-        }
+        context = super().get_context_data(**kwargs)
+        context['heroes'] = heroes
+        return context
 
-
-class StoreView(TemplateView):
-    template_name = 'heroes.html'
+# View to display an individual hero's profile
+class HeroDetailView(TemplateView):
+    template_name = 'hero.html'
 
     def get_context_data(self, **kwargs):
+        hero_name = self.kwargs.get('hero_name')
+        hero = heroes.get(hero_name, None)
+
+        if hero:
+            return hero
+        # Return a default context if hero is not found
         return {
-            'title': 'My Store',
-            'id': 'Interlock Go',
-            'image': '/static/interlockgo.jpeg'
+            'title': 'Hero Not Found',
+            'id': '',
+            'strengths': '',
+            'weaknesses': '',
+            'image': '/static/default.jpeg'
         }
