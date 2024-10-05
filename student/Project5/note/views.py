22fdf2ed8b75c25d0a04270f0b586ad248cb0613
InterlockGoEvans <cord3576@bears.unco.edu>
2024-10-04 20:51:52 -0600

    created templates

--- student/Project5/note/views.py
+++ student/Project5/note/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from markdown import markdown
 
-# Create your views here.
+from .models import Note
+
+
+class NoteListView(TemplateView):
+    template_name = 'notes.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'object_list': Note.objects.all()
+        }
+
+
+class NoteDetailView(TemplateView):
+    template_name = 'note.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'note': Note.objects.get(pk=kwargs['pk'])
+        }
