bf11e3a2423635997b7ba8167fbdb51c47b90b00
InterlockGoEvans <cord3576@bears.unco.edu>
2024-10-04 20:34:43 -0600

    extract the static files

--- student/Project5/note/models.py
+++ student/Project5/note/models.py
@@ -7,3 +7,6 @@ class Note(models.Model):  # Create a Note model that inherits from Django's bas
     title = models.CharField(max_length=200)   # A title field with a maximum length of 200 characters
     author = models.CharField(max_length=200)  # An author field with a maximum length of 200 characters
     body = models.TextField()                  # A body field for the main content, allows unlimited text
+
+def __str__(self):  # Define a string representation for each instance of the Note model
+    return f'{self.pk}. {self.title} - {self.author}'  # Return the title of the note
