a6084dfbfff9aff45af9fd7823a6a92a2301f542
InterlockGoEvans <cord3576@bears.unco.edu>
2024-10-14 22:42:57 -0600

    added some files

--- student/Project6/hero/urls.py
+++ student/Project6/hero/urls.py
@@ -3,19 +3,16 @@ from .views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView,
 from django.contrib import admin
 
 urlpatterns = [
-
-    # Hero
-    path('',                HeroListView.as_view(),    name='hero_list'),
-    path('<int:pk>',        HeroDetailView.as_view(),  name='hero_detail'),
-    path('add',             HeroCreateView.as_view(),  name='hero_add'),
-    path('<int:pk>/',       HeroUpdateView.as_view(),  name='hero_edit'),
-    path('<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
+    # Hero Views
+    path('',           HeroListView.as_view(),    name='hero_list'),          # Hero list
+    path('<int:pk>/',       HeroDetailView.as_view(),  name='hero_detail'),        # Hero detail
+    path('add/',            HeroCreateView.as_view(),  name='hero_add'),           # Add hero
+    path('<int:pk>/edit/',  HeroUpdateView.as_view(),  name='hero_edit'),          # Edit hero
+    path('<int:pk>/delete/',HeroDeleteView.as_view(),  name='hero_delete'),        # Delete hero
 
     # Login/Logout code
     path('accounts/', include('django.contrib.auth.urls')),
 
     # Admin views for users
     path('admin/', admin.site.urls),
-    # path('admin/', include('admin.site.urls')),   Don't do this!
-
 ]
