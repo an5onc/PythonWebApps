eede66debc72f2f2a7a408ea49fe0d2d1a35acca
InterlockGoEvans <cord3576@bears.unco.edu>
2024-10-14 23:52:48 -0600

    able to edit or delete existing heroes

--- student/Project6/config/settings.py
+++ student/Project6/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'hero.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [],  # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
