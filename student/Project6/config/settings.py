9598e8918c6ea2e05f30aff811ca1cd24bac78b6
InterlockGo <cord3576@bears.unco.edu>
2024-10-16 14:48:38 -0600

    debug back to false

--- student/Project6/config/settings.py
+++ student/Project6/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--bbs68lvsrx1n2n^s!)0wb7(yd4tkio&u4do46r&v741=-lunk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '165.22.144.128', '*']
 
