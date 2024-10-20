2f0ad7e9c5597bcfba225f5c98e7b18eacb25a9e
InterlockGoEvans <cord3576@bears.unco.edu>
2024-10-19 20:55:49 -0600

    add templates dir and updats views, models, and settings file

--- student/Project7/superhero/views.py
+++ student/Project7/superhero/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Superhero
+
+
+class HeroListView(ListView):
+    template_name = 'hero/list.html'
+    model = Superhero
+    context_object_name = 'heroes'
+
+class HeroDetailView(DetailView):
+    template_name = 'hero/detail.html'
+    model = Superhero
+    context_object_name = 'hero'
+
+class HeroCreateView(LoginRequiredMixin, CreateView):
+    template_name = "hero/add.html"
+    model = Superhero
+    fields = '__all__'
+
+
+class HeroUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "hero/edit.html"
+    model = Superhero
+    fields = '__all__'
+
+
+class HeroDeleteView(LoginRequiredMixin, DeleteView):
+    model = Superhero
+    template_name = 'hero/delete.html'
+    success_url = reverse_lazy('hero_list')
