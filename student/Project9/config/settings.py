1fd882d1248c5d922bd7d9dd5b659c938c712427
InterlockGoEvans <cord3576@bears.unco.edu>
2024-11-07 01:16:15 -0700

    changed settings

--- student/Project9/config/settings.py
+++ student/Project9/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u1d3!uog$uzs+28v(5ji4qw@!mdsndn$4e(+@!m#k4c^rfg8w7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.22.144.128']
 
 
 # Application definition
