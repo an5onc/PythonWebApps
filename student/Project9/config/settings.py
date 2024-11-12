6442768c2d72cf92455be65c48248b885219e6de
InterlockGoEvans <cord3576@bears.unco.edu>
2024-11-11 20:27:55 -0700

    settings

--- student/Project9/config/settings.py
+++ student/Project9/config/settings.py
@@ -73,7 +73,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# settings.py
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
